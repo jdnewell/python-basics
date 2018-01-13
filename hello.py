cd775293930164805064af9d02e4561d04563533
John <35179881+jdnewell@users.noreply.github.com>
2018-01-13 14:44:29 -0800

    add hello.py
    add hello world program

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/Users/jdnewell/anaconda3/bin python3.6
+# above code line lets this script run from command line by simply
+# typing the program hello.py from within the directory of hello.py
+
+# hello.py
+# john d newell jr
+# 01012018
+
+"""
+This program defines a main() function that prints the string "Hello World"
+"""
+
+
+def main():
+    message = "\nHello World\n"
+    print(message)
+
+
+main()
